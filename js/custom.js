b2c777065e17675a5f55730b37dad2019ec3cc93
Rizwan Ansari <rizwangazi7466@gmail.com>
2024-11-22 17:25:03 +0530

    v 0.0.4

---
+++ js/custom.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener("scroll", function () {
+        let scroll = window.scrollY;
+        let scroll_top = document.querySelector(".scroll-top img");
+        let downloadBtn = document.getElementById("download-btn");
+        let contactNo = document.getElementById("contact-no");
+        let mainHeader = document.querySelector(".main-header");
+
+        if (scroll >= 60) {
+            mainHeader.classList.add("animate", "pos-fixed");
+            scroll_top?.classList.add("d-block");
+            scroll_top?.classList.remove("d-none");
+            downloadBtn.classList.remove("d-none");
+            downloadBtn.classList.add("d-block");
+            contactNo.classList.remove("d-block");
+            contactNo.classList.add("d-none");
+        } else {
+            mainHeader.classList.remove("animate", "pos-fixed");
+            scroll_top?.classList.remove("d-block");
+            scroll_top?.classList.add("d-none");
+            downloadBtn.classList.remove("d-block");
+            downloadBtn.classList.add("d-none");
+            contactNo.classList.remove("d-none");
+            contactNo.classList.add("d-block");
+        }
+    });
+});
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
